d679670b6246f0af7e7e4ded6476794cc561ffad
Jack Maders <jackwmaders@gmail.com>
2025-11-14 18:07:42 +0000

    feat: add payload cms

---
+++ payload.config.ts
@@ -0,0 +1,20 @@
+import { postgresAdapter } from "@payloadcms/db-postgres";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import { buildConfig } from "payload";
+import sharp from "sharp";
+
+const connectionString = process.env.PAYLOAD_CONNECTION_STRING ?? "";
+const secret = process.env.PAYLOAD_SECRET ?? "";
+
+export default buildConfig({
+	editor: lexicalEditor(),
+	collections: [],
+	secret,
+	debug: true,
+	db: postgresAdapter({
+		push: true,
+		pool: { connectionString },
+		migrationDir: "db/migrations",
+	}),
+	sharp,
+});
