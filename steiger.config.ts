66d8af7fad7eff10e07caa69d856a531c33c61b5
Jack Maders <jackwmaders@gmail.com>
2025-11-14 18:18:02 +0000

    chore: ignore payload files from architecture check

---
+++ steiger.config.ts
@@ -0,0 +1,12 @@
+import fsd from "@feature-sliced/steiger-plugin";
+import { defineConfig } from "steiger";
+
+export default defineConfig([
+  ...fsd.configs.recommended,
+  {
+    files: ["./src/app/**/*payload*/**"],
+    rules: {
+      "fsd/no-reserved-folder-names": "off",
+    },
+  },
+]);
