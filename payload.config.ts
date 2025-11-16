499136d5d039bfcc06a0a1874cb30018ca44341e
Jack Maders <jackwmaders@gmail.com>
2025-11-16 10:13:24 +0000

    chore: fix formatting

--- payload.config.ts
+++ payload.config.ts
@@ -9,64 +9,64 @@ const connectionString = process.env.STORAGE_POSTGRES_URL ?? "";
 const secret = process.env.PAYLOAD_SECRET ?? "";
 
 export default buildConfig({
-  editor: lexicalEditor(),
-  collections: [],
-  secret,
-  debug: true,
-  db: postgresAdapter({
-    push: true,
-    pool: { connectionString },
-    migrationDir: "db/migrations",
-  }),
-  sharp,
-  plugins: [
-    betterAuthPlugin({
-      betterAuthOptions: {
-        emailAndPassword: {
-          enabled: true,
-        },
+	editor: lexicalEditor(),
+	collections: [],
+	secret,
+	debug: true,
+	db: postgresAdapter({
+		push: true,
+		pool: { connectionString },
+		migrationDir: "db/migrations",
+	}),
+	sharp,
+	plugins: [
+		betterAuthPlugin({
+			betterAuthOptions: {
+				emailAndPassword: {
+					enabled: true,
+				},
 
-        plugins: [nextCookies()],
-        user: {
-          additionalFields: {
-            role: {
-              type: "string",
-              defaultValue: "user",
-              input: false,
-            },
-          },
-        },
-      },
-      disableDefaultPayloadAuth: true,
-      users: {
-        slug: "users",
-        collectionOverrides: ({ collection }) => ({
-          ...collection,
-          timestamps: true,
-          fields: [...collection.fields],
-        }),
-      },
-      accounts: {
-        collectionOverrides: ({ collection }) => ({
-          ...collection,
-          timestamps: true,
-        }),
-      },
-      sessions: {
-        collectionOverrides: ({ collection }) => ({
-          ...collection,
-          timestamps: true,
-        }),
-      },
-      verifications: {
-        collectionOverrides: ({ collection }) => ({
-          ...collection,
-          timestamps: true,
-        }),
-      },
-    }),
-  ],
-  typescript: {
-    outputFile: "src/shared/model/payload/types.d.ts",
-  },
+				plugins: [nextCookies()],
+				user: {
+					additionalFields: {
+						role: {
+							type: "string",
+							defaultValue: "user",
+							input: false,
+						},
+					},
+				},
+			},
+			disableDefaultPayloadAuth: true,
+			users: {
+				slug: "users",
+				collectionOverrides: ({ collection }) => ({
+					...collection,
+					timestamps: true,
+					fields: [...collection.fields],
+				}),
+			},
+			accounts: {
+				collectionOverrides: ({ collection }) => ({
+					...collection,
+					timestamps: true,
+				}),
+			},
+			sessions: {
+				collectionOverrides: ({ collection }) => ({
+					...collection,
+					timestamps: true,
+				}),
+			},
+			verifications: {
+				collectionOverrides: ({ collection }) => ({
+					...collection,
+					timestamps: true,
+				}),
+			},
+		}),
+	],
+	typescript: {
+		outputFile: "src/shared/model/payload/types.d.ts",
+	},
 });
--- src/shared/model/auth/index.ts
+++ src/shared/model/auth/index.ts
@@ -4,7 +4,7 @@ import type { BetterAuthReturn } from "payload-auth/better-auth";
 import { payload } from "../payload";
 
 export type Payload = BasePayload & {
-  betterAuth: BetterAuthReturn<Array<ReturnType<typeof nextCookies>>>;
+	betterAuth: BetterAuthReturn<Array<ReturnType<typeof nextCookies>>>;
 };
 
 export const { api, handler } = (payload as Payload).betterAuth;
